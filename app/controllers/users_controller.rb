d0205418f6eed4ac4e844d33f926a1b192979e4a
juliaamik15 <juliaamik15@gmail.com>
2016-09-29 13:49:45 +0100

    cancan added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  load_and_authorize_resource :except => [:show, :index]
   # GET /users
   # GET /users.json
   def index
