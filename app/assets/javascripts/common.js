9de67ff60db2bfba4b6fc098e21ed90083b83de6
juliaamik15 <juliaamik15@gmail.com>
2016-12-03 20:43:25 +0000

    styles for index page

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,7 +1,8 @@
 $(window).scroll(function(){
 	var st= $(this).scrollTop();
-	$(".section_title").css({
-		"transform" : "translate(-50%, "+ st*1.5 +"%"
 
+	$(".header_text").css({
+		"transform" : "translate(-180px-50%, "+ st*2 +"%)"
 	});
+	
 });
